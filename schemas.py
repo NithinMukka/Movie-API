93cc036cfd4ce44d725309a01494083e9225ac1a
NithinMukka <nithinmukka51234@gmail.com>
2026-06-23 11:35:44 +0530

    feat: add field validator to MovieCreate schema to normalize show_time to naive datetime

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-from datetime import datetime
+from pydantic import BaseModel, field_validator
+from datetime import datetime, timezone
 from typing import Optional
 
 # Data expected from user to create/update a movie
@@ -8,6 +8,13 @@ class MovieCreate(BaseModel):
     description: str
     show_time: datetime
     available_seats: int
+
+    @field_validator("show_time")
+    @classmethod
+    def make_show_time_naive(cls, v: datetime) -> datetime:
+        if v.tzinfo is not None:
+            return v.astimezone(timezone.utc).replace(tzinfo=None)
+        return v
 
 # Data returned to the user
 class MovieResponse(BaseModel):
